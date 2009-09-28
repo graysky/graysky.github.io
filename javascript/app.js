00d574d08cab822b4beeeb33d7d11aee8a08fa20
Mike Champion <mike@graysky.org>
2009-09-28 01:24:00 -0400

    about me pages tweaks

--- javascript/app.js
+++ javascript/app.js
@@ -25,7 +25,7 @@ function fixPNG(myImage)
 
 // Write out contact line
 function get_contact_sig() {
-  document.write("Email: " + get_contact_email() );
+  document.write(get_contact_email() );
 }
 
 // Hide email address a bit
@@ -42,4 +42,15 @@ function get_contact_email_link() {
    var b = "graysky";
    var c = ".org";
    return "<a href=mailto:"+a+"@"+b+c+">";
+}
+
+// Pic a random picture
+function pic_thumbnail() {
+	var pics = new Array();
+	pics[0] = "/images/thumbnails/charles_t.jpg";
+	pics[1] = "/images/thumbnails/mit_dorm.jpg";
+	pics[2] = "/images/thumbnails/macallen_building.jpg";
+	var choice = Math.floor(Math.random() * pics.length);
+	var img_tag = "<img src=" + pics[choice] + " style='border: 1px solid black;'>";
+	document.write(img_tag);
 }
